c422e9f66d6518d76f103d3b2fbec947cd8dc81b
Arthur <49246305+KiYari@users.noreply.github.com>
2021-03-01 19:52:01 +1000

    111

---
+++ src/Layout/VideoComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import {Container, Grid, Typography} from '@material-ui/core';
+import ReactPlayer from 'react-player/youtube'
+import Header from './Header'
+import Nav from './Nav';
+
+const useStyles = theme => ({
+  cont: {
+    marginLeft: 24,
+    backgroundColor: 'red'
+  }
+});
+
+class VideoComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      vis: [],
+      vinf: {},
+      comments: [],
+      logo: '',
+    }
+  }
+  vid_inf = (props) => {
+    fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id='+(window.location.href).split("/")[4]+'&key=AIzaSyDChwSyYbFWJl2LJOgBbSwBcs6WAxb9Qv4').then(response => response.json()).then(data => {
+      var _data = {id: 0, channelTitle: data['items'][0]['snippet']['channelTitle'],
+        title: data['items'][0]['snippet']['title'],
+        publishedAt: data['items'][0]['snippet']['publishedAt'],
+        desc: data['items'][0]['snippet']['localized']['description'],
+      channelId: data['items'][0]['snippet']['channelId']};
+      this.setState({vinf: _data});
+    }).catch(err => alert("Wrong!ffff"))
+  }
+
+  render() {
+    const {classes} = this.props;
+
+    return (<div>
+      <Header>
+        <Nav/>
+      </Header>
+      <div className={classes.mainContainer}>
+        <ReactPlayer width="50vw" height="50vh" url={'https://www.youtube.com/watch?v=' + (window.location.href).split("/")[4]}/>
+      </div>
+
+    </div>)
+  }
+}
+
+export default withStyles(useStyles)(VideoComponent)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import NotFound from './Pages/NotFound';
+import Video from './Pages/Video'
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//:id
+ReactDOM.render(<Router>
+  <Switch>
+    <Route exact="exact" path="/"><Redirect to="/video/9Il5H51_37o" /></Route>
+    <Route path="/video/:index"><Video/></Route>
+    <Route><NotFound/></Route>
+  </Switch>
+</Router>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
